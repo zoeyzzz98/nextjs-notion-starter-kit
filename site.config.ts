c1cfdfd09763fc79ad57b37e00411972dc42fc16
zoeyzzz98 <109580176+zoeyzzz98@users.noreply.github.com>
2022-07-19 15:25:57 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '7b742c7088e84247bacdbb1284100a35',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Zoey design',
+  domain: 'https://slash-peace-078.notion.site/7b742c7088e84247bacdbb1284100a35',
+  author: 'Zoey',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
